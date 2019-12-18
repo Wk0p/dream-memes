d8832f8d60b81f76936939934bd9cfc3884cea3c
Wk0p <57257114+Wk0p@users.noreply.github.com>
2019-12-18 00:50:18 +0000

    JS button test
    Read More test

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,23 @@
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
+<script type="text/javascript">
+var i=0; 
+	function read(){
+		if(!i){
+document.getElementByID("more").style.
+		display = "inline";
+document.getElementByID("dots").style.
+		display = "none";
+document.getElementByID("read more").innerHTML="Read Less";
+		i=1;
+	}
+	else {
+	document.getElementByID("more").style.
+		display = "none";
+document.getElementByID("dots").style.
+		display = "inline";
+document.getElementByID("read more").innerHTML="Read more";
+		i=0;
+	
+	
+	}
+	}
+</script>
