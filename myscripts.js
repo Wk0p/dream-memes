5905f1bd8b3d16ffa803860846d8d4c04bb7c025
Wk0p <57257114+Wk0p@users.noreply.github.com>
2019-12-18 00:17:45 +0000

    New - fixed html/css
    CSS Reset - fixed
    Nav - fixed
    Images - fixed
    H's / P - fixed
    
    JS - Read more button broken: button there but 'read more' text appearing

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,15 @@
-//show thank you message
-      submit.attr('value', 'Thank you :)')
-            .css({boxShadow: 'none'});
-    }, 2000);
-    setTimeout(function() {
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
-});
+}
